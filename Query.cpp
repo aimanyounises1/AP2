e532d6c38f49869b3ab6ad714578dc96befa68fd
aimanyounises1 <aymanyounis@icloud.com>
2020-11-25 09:20:44 +0200

    adas

--- Query.cpp
+++ Query.cpp
@@ -12,20 +12,27 @@ using namespace std;
 /////////////////////////////////////////////////////////
 shared_ptr<QueryBase> QueryBase::factory(const string &s)
 {
-    
+    int count = 0;
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            count++;
+        }
+    }   
     string sText;
     string s1;
     string s2;
-    if (s.substr(0, 3) == "AND" && s.at(3) == ' ')
+    bool ans = !(s1.empty() && s2.empty());
+    if (s.substr(0, 3) == "AND" && s.at(3) == ' '&& count == 2 )
     {
         sText = s.substr(4, s.length());
         size_t i = sText.find(" ");
         s1 = sText.substr(0, i);
         s2 = sText.substr(i + 1, s.length());
-        cout << "s1 is "<< s1 << " s2 is  = "<< s2<< endl;
         return std::shared_ptr<QueryBase>(new AndQuery(s1, s2));
     }
-    else if (s.substr(0, 2) == "OR" && s.at(2) == ' ')
+    else if (s.substr(0, 2) == "OR" && s.at(2) == ' ' && count  == 2)
     {
         sText = s.substr(3, s.length());
         size_t i = sText.find(" ");
@@ -33,7 +40,7 @@ shared_ptr<QueryBase> QueryBase::factory(const string &s)
         s2 = sText.substr(i + 1, s.length());
         return std::shared_ptr<QueryBase>(new OrQuery(s1, s2));
     }
-    else if (s.substr(0, 2) == "AD" && s.at(2) == ' ')
+    else if (s.substr(0, 2) == "AD" && s.at(2) == ' '&&count  == 2)
     {
         sText = s.substr(3, s.length());
         size_t i = sText.find(" ");
@@ -43,11 +50,12 @@ shared_ptr<QueryBase> QueryBase::factory(const string &s)
     }
     else if (s.find(" ") == -1)
     {
-      return std::shared_ptr<QueryBase>(new WordQuery(s));
+        return std::shared_ptr<QueryBase>(new WordQuery(s));
         //exit(1);
-    }else{
-        cout << "Unrecognized search" << endl;
-      //  return NULL;
+    }
+    else
+    {
+        return std::shared_ptr<QueryBase>(new WordQuery("Unrecognized search"));
     }
 }
 QueryResult AndQuery::eval(const TextQuery &text) const
@@ -58,7 +66,7 @@ QueryResult AndQuery::eval(const TextQuery &text) const
     set_intersection(left_result.begin(), left_result.end(),
                      right_result.begin(), right_result.end(),
                      inserter(*ret_lines, ret_lines->begin()));
-    
+
     return QueryResult(rep(), ret_lines, left_result.get_file());
 }
 QueryResult OrQuery::eval(const TextQuery &text) const
@@ -84,7 +92,7 @@ QueryResult AdjacentQuery::eval(const TextQuery &text) const
             if (i == j + 1)
             {
                 ret->insert(i);
-                ret->insert(j );
+                ret->insert(j);
                 break;
             }
             else if (i == j - 1)
@@ -95,18 +103,49 @@ QueryResult AdjacentQuery::eval(const TextQuery &text) const
             }
         }
     }
-    return QueryResult(rep(),ret, left_result.get_file());
+    return QueryResult(rep(), ret, left_result.get_file());
 }
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
 std::ostream &print(std::ostream &os, const QueryResult &qr)
 {
-    os << "\"" << qr.sought << "\""
-       << " occurs " << qr.lines->size() << " times:" << std::endl;
-    for (auto num : *qr.lines)
+    string s = qr.sought;
+    size_t i = s.find(" ");
+    if (s == "Unrecognized search")
     {
-        os << "\t(line " << num + 1 << ") "
-           << *(qr.file->begin() + num) << std::endl;
+        os<< "Unrecognized search";
+    }
+   else if (s.substr(0, i) == "AD")
+    {
+        int count =  0;
+        std::vector <int> v;
+        for (auto i : *qr.lines)
+        {
+           v.push_back(i);
+        }
+       for(int i = 0 ;i < v.size() -1; i++){
+           if (v[i] + 1 == v[i+1])
+           {
+               count++;
+           }
+       }
+        os << "\"" << qr.sought << "\""
+           << " occurs " << count << " times:" << std::endl;
+        for (auto num : *qr.lines)
+        {
+            os << "\t(line " << num + 1 << ") "
+               << *(qr.file->begin() + num) << std::endl;
+        }
+    }
+    else
+    {
+        os << "\"" << qr.sought << "\""
+           << " occurs " << qr.lines->size() << " times:" << std::endl;
+        for (auto num : *qr.lines)
+        {
+            os << "\t(line " << num + 1 << ") "
+               << *(qr.file->begin() + num) << std::endl;
+        }
     }
     return os;
 }
